3d1eaa038ee06b5ba6c40671fee5dd1d0bf0f887
Tim <schnell.tim@gmail.com>
2023-09-12 17:27:07 +0200

    Replace HTML with markdown renderer in faq

--- components/EntryModal.vue
+++ components/EntryModal.vue
@@ -8,7 +8,7 @@
       <h1 class="font-bold text-xl mb-2 border-b-2 border-gray-200">
         {{ title }}
       </h1>
-      <p v-html="text" />
+      <Markdown :source="text" class="markdown-body" />
       <button
         @click="closeModal"
         class="rounded text-gray-200 hover:bg-red-400 rounded p-2 ml-1 mt-2 float-right bg-red-600"
@@ -20,14 +20,8 @@
 </template>
 
 <script setup lang="ts">
-import {
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-  TransitionChild,
-  TransitionRoot,
-} from "@headlessui/vue";
 import { useFilterStore } from "~/stores/filters";
+import Markdown from "vue3-markdown-it";
 
 const props = defineProps<{
   title: string;
