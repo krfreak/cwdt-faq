3539c8a6d7aaa2da6dc98abc78c794d4bd0016ea
Kevin Wardenga <kevin.wardenga@googlemail.com>
2023-09-17 19:09:41 +0200

    feat: improve modals
    chore: move markdown styles into own file
    chore: use scss for main styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['@/assets/css/main.css'],
+  css: ['@/assets/css/main.scss', '@/assets/css/markdown.css'],
   devtools: { enabled: true },
   modules: ['@nuxtjs/eslint-module', '@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxt/content'],
   content: {},
