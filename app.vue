15f09ef396e69e14de318a8c1c7ce51aea604f4e
Tim <schnell.tim@gmail.com>
2023-09-18 14:13:27 +0200

    Add more FAQ content

--- app.vue
+++ app.vue
@@ -10,8 +10,8 @@
     </section>
     <footer class="mx-2 px-2 mt-auto z-100">
       <div class="flex space-x-4">
-        <a href="https://discord.gg/jp9vQeHgWY" class="rounded-t-lg ring-2 ring-amber-500 p-2 bg-zinc-700 hover:bg-zinc-900">Discord</a>
-        <a href="https://returnx.github.io/cwdt/" class="rounded-t-lg ring-2 ring-amber-500 p-2 bg-zinc-700 hover:bg-zinc-900">Calculator</a>
+        <a href="https://discord.gg/jp9vQeHgWY" class="rounded-t-lg ring-2 ring-amber-500 p-2 bg-zinc-700 hover:bg-zinc-900" target="_blank">Discord</a>
+        <a href="https://returnx.github.io/cwdt/" class="rounded-t-lg ring-2 ring-amber-500 p-2 bg-zinc-700 hover:bg-zinc-900" target="_blank">Calculator</a>
       </div>
     </footer>
   </div>
